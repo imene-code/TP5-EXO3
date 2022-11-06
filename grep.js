e8961b5f35914d9f56bb86c1d709121dac64884e
imene-code <115371223+imene-code@users.noreply.github.com>
2022-11-06 21:53:56 +0000

    Update grep.js

--- grep.js
+++ grep.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
-
+const path = require('path');
 function Grep(regEx,...Files){
    
     const regex = new RegExp(regEx);
-    
+    const found = []
     for(f in Files){
 
         const isDir = fs.statSync(`./${Files[f]}`).isDirectory();
         
         if(!isDir){
-            const data = fs.readFileSync(`${Files[f]}`, 'utf8');		
+            const data = fs.readFileSync(`${Files[f]}`, 'utf8');
             if(regex.test(data)){
                 
-                return `found in: ${Files[f]}`;
+                found.push(Files[f])
             }
         }else{
             let recFiles = fs.readdirSync(Files[f]);
@@ -20,11 +20,15 @@ function Grep(regEx,...Files){
                 recFiles[file] = `./${Files[f]}/${recFiles[file]}`;
             }
             
-            return Grep(regEx,...recFiles);
+               let res = Grep(regEx,...recFiles);
+               res.forEach((e)=>{
+                found.push(path.basename(e))
+               })
         }
         
 
     }
-    return "Not found";
+    
+    return found? found :"NONE";
 }
 console.log(Grep("aze",'tstA','tstB','tstC','D'))
